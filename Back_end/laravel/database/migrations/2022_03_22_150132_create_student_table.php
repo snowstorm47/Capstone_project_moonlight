a096a1f0f2b3ae4f53be76e2c95d18fbb5e232d0
Abigya Getachew <Abiguget18@gmail.com>
2022-03-22 18:11:02 +0300

    added student table

---
+++ Back_end/laravel/database/migrations/2022_03_22_150132_create_student_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('institution_id')->constrained('institution');
+            $table->string('phoneNumber');
+            $table->string("sex");
+            $table->string("major");
+            $table->date("startDate");
+            $table->date("endDate");
+            $table->string("GPA");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student');
+    }
+};
