5ea866a3585ba495611a7b9c9e17cbcaee4bb797
Abigya Getachew <Abiguget18@gmail.com>
2022-03-23 09:34:46 +0300

    added seeder to tables created by abigya only hiring company and students don't have seeders and created tables

---
+++ Back_end/laravel/database/seeders/usersSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+class usersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name'=> 'Abebe Tesema',
+            'email'=>'AbebeTesema@gmail.com',
+            'password'=>'1234',
+            'position'=>'student'
+        ]);
+    }
+}
