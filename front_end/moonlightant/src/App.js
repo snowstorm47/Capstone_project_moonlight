fb24a49eb73e8bf855af4061419886142c6ebcbc
Abigya Getachew <Abiguget18@gmail.com>
2022-04-18 14:14:46 +0300

    added no change

--- front_end/moonlightant/src/App.js
+++ front_end/moonlightant/src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import AboutUs from './aboutUs';
+// import AboutUs from './AboutUs';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+      {/* <Route path='/aboutus' element= { <AboutUs />  }/> */}
+
+      </Routes>
+    </Router>
   );
 }
 
