79701256b532807a0febc0c06151cc087240f606
Abigya Getachew <Abiguget18@gmail.com>
2022-04-19 21:49:49 +0300

    added about us and contact us page

--- front_end/moonlightant/src/App.js
+++ front_end/moonlightant/src/App.js
@@ -1,16 +1,22 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Newsfeed from "./pages/newsFeed";
-
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import 'antd/dist/antd.css';
 function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/newsfeed" element={<Newsfeed />} />
-			</Routes>
-		</div>
-	);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/newsfeed" element={<Newsfeed />} />
+          <Route path="/aboutus" element={<AboutUs />} />
+          <Route path="/contactus" element={<ContactUs />} />
+        </Routes>
+      </Router>
+    </div>
+  );
 }
 
 export default App;
