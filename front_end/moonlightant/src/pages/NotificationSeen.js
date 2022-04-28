bb120bad9acc38625ff00a36d69590b2b547ac9b
Abigya Getachew <Abiguget18@gmail.com>
2022-04-28 23:52:26 +0300

    edited notification page

--- front_end/moonlightant/src/pages/NotificationSeen.js
+++ front_end/moonlightant/src/pages/NotificationSeen.js
@@ -1,5 +1,6 @@
 import { Component, useState } from "react";
-import { Layout, Menu, Space, Alert, Row, Col,Button } from "antd";
+import { Layout, Menu, Space, Alert, Row, Col,Button, List,
+  Avatar,} from "antd";
 import {
   DashboardOutlined,
   BankOutlined,
@@ -7,9 +8,18 @@ import {
   UserOutlined,
   PlusCircleOutlined,
   CheckOutlined,
+  CloseOutlined,
 } from "@ant-design/icons";
 const { Header, Footer, Sider, Content } = Layout;
 
+const data = [
+  {
+    title: "Ant Design Title 1",
+  },
+  {
+    title: "Ant Design Title 2",
+  },
+];
 // Introduce submenu components
 const SubMenu = Menu.SubMenu;
 
@@ -22,19 +32,53 @@ const NotificationSeen = () => {
   return (
    <div>
         {visible ? (
-          <Alert
-            message="Seen Notifications"
-            type="info"
-            action={
-              <Space>
-                <Button size="small" type="text">
-                  <CheckOutlined size="small" type="ghost" />
-                </Button>
-              </Space>
-            }
-            closable
-            afterClose={handleClose}
-          />
+          <List
+          itemLayout="horizontal"
+          dataSource={data}
+          renderItem={(item) => (
+            <List.Item
+            style={{
+              marginBottom:"1em"
+            }}
+            >
+              <List.Item.Meta
+                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+                title={
+                  <a
+                    href="#"
+                    style={{
+                      marginLeft: "0em",
+                    }}
+                  >
+                    {item.title}
+                  </a>
+                }
+                description=
+                "Ant Design, a design language for background applications, is refined by Ant UED Team "
+                style={{
+                  textAlign:"left"
+                }}
+              />
+              <br/>
+              <br/>
+              <div
+              style={{
+                marginTop:"5em"
+              }}
+              >Apr 28, 2022</div>
+              <List.Item
+                actions={[
+                  <a key="list-loadmore-edit">
+                    <CheckOutlined />
+                  </a>,
+                  <a key="list-loadmore-more">
+                    <CloseOutlined />
+                  </a>,
+                ]}
+              ></List.Item>
+            </List.Item>
+          )}
+        />
         ) : null}
      </div>
   );
